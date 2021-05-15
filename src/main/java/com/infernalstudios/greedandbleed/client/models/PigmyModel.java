399d6e1c0d96a18a7e7c09c364dd2a676501efd7
Thelnfamous1 <miguel.garay60622@gmail.com>
2021-05-15 05:19:48 -0500

    Mostly fixed the pigmy's model, just need to fix the ears. Should consider remaking this in Tabula, as BlockBench isn't that great for mobs.

--- src/main/java/com/infernalstudios/greedandbleed/client/models/PigmyModel.java
+++ src/main/java/com/infernalstudios/greedandbleed/client/models/PigmyModel.java
@@ -18,11 +18,11 @@ import net.minecraft.util.math.MathHelper;
 public class PigmyModel<T extends MobEntity> extends BipedModel<T> {
 	public final ModelRenderer earRight;
 	public final ModelRenderer earLeft;
+	private final ModelRenderer hair;
 	private final ModelRenderer bodyDefault;
 	private final ModelRenderer headDefault;
 	private final ModelRenderer leftArmDefault;
 	private final ModelRenderer rightArmDefault;
-	private final ModelRenderer hair;
 
 	public PigmyModel(float modelSize, int texWidthIn, int textHeightIn) {
 		super(modelSize);
@@ -31,76 +31,64 @@ public class PigmyModel<T extends MobEntity> extends BipedModel<T> {
 
 		this.body = new ModelRenderer(this);
 		this.body.setPos(0.0F, 24.0F, 0.0F);
-		ModelRenderer upperbody = new ModelRenderer(this);
-		upperbody.setPos(0.0F, 0.0F, 0.0F);
-		this.body.addChild(upperbody);
-		upperbody.texOffs(0, 16).addBox(-4.0F, -16.0F, -3.0F, 8.0F, 10.0F, 6.0F, 0.0F, false);
+		this.body.texOffs(0, 16)
+				.addBox(-4.0F, -16.0F + 16.0F + 7.0F, -3.0F, 8.0F, 10.0F, 6.0F, modelSize, false);
 
 		this.head = new ModelRenderer(this);
-		this.head.setPos(0.0F, 0.0F, 0.0F);
-		this.head.texOffs(0, 0).addBox(-5.0F, -24.0F, -4.0F, 10.0F, 8.0F, 8.0F, 0.0F, false);
-		//this.body.addChild(headpieces);
-
-		this.hair = new ModelRenderer(this);
-		this.hair.setPos(0.0F, 0.0F, 0.0F);
-		this.head.addChild(this.hair);
-		this.hair.texOffs(0, 22).addBox(0.0F, -30.0F, -4.0F, 0.0F, 12.0F, 10.0F, 0.0F, false);
-
-		//ears = new ModelRenderer(this);
-		//ears.setPos(0.0F, 0.0F, 0.0F);
-		//this.head.addChild(ears);
-
-		this.earLeft = new ModelRenderer(this);
-		this.earLeft.setPos(-6.0F, -17.0F, -3.0F);
-		//ears.addChild(earRight);
-		this.head.addChild(this.earLeft);
-		setRotationAngle(this.earLeft, 0.0F, 0.0F, 0.1745F);
-		this.earLeft.texOffs(39, 26).addBox(-1.0F, -6.0F, 0.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
-
-		this.earRight = new ModelRenderer(this);
-		this.earRight.setPos(7.0F, -17.0F, -3.0F);
-		//ears.addChild(earLeft);
-		this.head.addChild(this.earRight);
-		setRotationAngle(this.earRight, 0.0F, 0.0F, -0.1745F);
-		this.earRight.texOffs(14, 38).addBox(-1.0F, -6.0F, 0.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+		this.head.setPos(0.0F, 24.0F, 0.0F);
+		this.head.texOffs(0, 0)
+				.addBox(-5.0F, -24.0F + 16.0F + 7.0F, -4.0F, 10.0F, 8.0F, 8.0F, modelSize, false);
 
 		ModelRenderer snout = new ModelRenderer(this);
 		snout.setPos(0.0F, 0.0F, 0.0F);
 		this.head.addChild(snout);
-		snout.texOffs(42, 10).addBox(-3.0F, -20.0F, -5.0F, 6.0F, 4.0F, 1.0F, 0.0F, false);
+		snout.texOffs(42, 10)
+				.addBox(-3.0F, -20.0F + 16.0F + 7.0F, -5.0F, 6.0F, 4.0F, 1.0F, modelSize, false);
+		snout.texOffs(0, 0)
+				.addBox(3.0F, -18.0F + 16.0F + 7.0F, -5.0F, 1.0F, 2.0F, 1.0F, modelSize, false);
+		snout.texOffs(0, 3)
+				.addBox(-4.0F, -18.0F + 16.0F + 7.0F, -5.0F, 1.0F, 2.0F, 1.0F, modelSize, false);
 
-		ModelRenderer teeth = new ModelRenderer(this);
-		teeth.setPos(0.0F, 0.0F, 0.0F);
-		snout.addChild(teeth);
-		teeth.texOffs(0, 0).addBox(3.0F, -18.0F, -5.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
-		teeth.texOffs(0, 3).addBox(-4.0F, -18.0F, -5.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
+		this.earLeft = new ModelRenderer(this);
+		this.earLeft.setPos(-6.0F, -17.0F, -3.0F);
+		this.head.addChild(this.earLeft);
+		this.setRotationAngle(this.earLeft, 0.0F, 0.0F, 0.1745F);
+		this.earLeft.texOffs(39, 26)
+				.addBox(-1.0F + 10.0F, -6.0F + 16.0F + 7.0F, 0.0F, 1.0F, 6.0F, 6.0F, modelSize, false);
 
-		//arms = new ModelRenderer(this);
-		//arms.setPos(0.0F, 0.0F, 0.0F);
-		//upperbody.addChild(arms);
-		//arms.texOffs(31, 11).addBox(4.0F, -16.0F, -2.0F, 3.0F, 10.0F, 5.0F, 0.0F, false);
-		//arms.texOffs(23, 27).addBox(-7.0F, -16.0F, -2.0F, 3.0F, 10.0F, 5.0F, 0.0F, false);
+		this.earRight = new ModelRenderer(this);
+		this.earRight.setPos(7.0F, -17.0F, -3.0F);
+		this.head.addChild(this.earRight);
+		this.setRotationAngle(this.earRight, 0.0F, 0.0F, -0.1745F);
+		this.earRight.texOffs(14, 38)
+				.addBox(-1.0F - 10.0F, -6.0F + 16.0F + 7.0F, 0.0F, 1.0F, 6.0F, 6.0F, modelSize, false);
+
+		this.hair = new ModelRenderer(this);
+		this.hair.setPos(0.0F, 0.0F, 0.0F);
+		this.head.addChild(this.hair);
+		this.hair.texOffs(0, 22)
+				.addBox(0.0F, -30.0F + 16.0F + 7.0F, -4.0F, 0.0F, 12.0F, 10.0F, modelSize, false);
+
 		this.leftArm = new ModelRenderer(this);
-		//upperbody.addChild(leftArm);
-		this.leftArm.texOffs(31, 11).addBox(4.0F, -16.0F, -2.0F, 3.0F, 10.0F, 5.0F, 0.0F, false);
+		this.leftArm.setPos(0.0F, 24.0F, 0.0F);
+		this.leftArm.texOffs(31, 11).
+				addBox(4.0F - 5.0F, -16.0F + 14.0F + 7.0F, -2.0F, 3.0F, 10.0F, 5.0F, modelSize, false);
 
 		this.rightArm = new ModelRenderer(this);
-		//upperbody.addChild(rightArm);
-		this.rightArm.texOffs(23, 27).addBox(-7.0F, -16.0F, -2.0F, 3.0F, 10.0F, 5.0F, 0.0F, false);
+		this.rightArm.setPos(0.0F, 24.0F, 0.0F);
+		this.rightArm.texOffs(23, 27)
+				.addBox(-7.0F + 5.0F, -16.0F + 14.0F + 7.0F, -2.0F, 3.0F, 10.0F, 5.0F, modelSize, false);
 
-		//legs = new ModelRenderer(this);
-		//legs.setPos(0.0F, 0.0F, 0.0F);
-		//body.addChild(legs);
-		//legs.texOffs(35, 38).addBox(0.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
-		//legs.texOffs(36, 0).addBox(-4.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
 		this.leftLeg = new ModelRenderer(this);
-		//upperbody.addChild(leftLeg);
-		this.leftLeg.texOffs(35, 38).addBox(0.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+		this.leftLeg.setPos(0.0F, 24.0F, 0.0F);
+		this.leftLeg.texOffs(35, 38)
+				.addBox(0.0F, -6.0F + 4.0F + 7.0F, -2.0F, 4.0F, 6.0F, 4.0F, modelSize, false);
 
 		this.rightLeg = new ModelRenderer(this);
-		//upperbody.addChild(rightLeg);
-		this.rightLeg.texOffs(36, 0).addBox(-4.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
-
+		this.rightLeg.setPos(0.0F, 24.0F, 0.0F);
+		this.rightLeg.texOffs(36, 0)
+				.addBox(-4.0F, -6.0F + 4.0F + 7.0F, -2.0F, 4.0F, 6.0F, 4.0F, modelSize, false);
+		
 		this.hat = new ModelRenderer(this);
 
 		this.bodyDefault = this.body.createShallowCopy();
