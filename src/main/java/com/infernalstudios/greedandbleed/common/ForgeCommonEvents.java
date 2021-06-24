878b95f4d1ea30ce3ef3438965e93eaf07e5b4ab
Thelnfamous1 <miguel.garay60622@gmail.com>
2021-06-24 16:30:41 -0500

    Added skiglin spawns to soul sand valleys

--- src/main/java/com/infernalstudios/greedandbleed/common/ForgeCommonEvents.java
+++ src/main/java/com/infernalstudios/greedandbleed/common/ForgeCommonEvents.java
@@ -4,9 +4,14 @@ import com.infernalstudios.greedandbleed.common.entity.IToleratingMount;
 import com.infernalstudios.greedandbleed.common.entity.piglin.PigmyEntity;
 import com.infernalstudios.greedandbleed.common.entity.piglin.SkeletalPiglinEntity;
 import com.infernalstudios.greedandbleed.common.registry.EntityTypeRegistry;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -39,6 +44,15 @@ public class ForgeCommonEvents {
             else if(toleratingMount.getTolerance() < 0){
                 toleratingMount.setTolerance(0);
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onBiomeLoading(BiomeLoadingEvent event){
+        if(event.getName() != null && event.getName().equals(Biomes.SOUL_SAND_VALLEY.getRegistryName())){
+            MobSpawnInfo.Spawners skiglinSpawner = new MobSpawnInfo.Spawners(EntityTypeRegistry.SKELETAL_PIGLIN.get(), 20, 5, 5);
+            event.getSpawns().getSpawner(EntityClassification.MONSTER).add(skiglinSpawner);
+            event.getSpawns().addMobCharge(EntityTypeRegistry.SKELETAL_PIGLIN.get(), 0.7D, 0.15D);
         }
     }
 }
