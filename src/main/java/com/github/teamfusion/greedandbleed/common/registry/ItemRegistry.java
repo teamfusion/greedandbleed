59752f19d52b72616a397eb338a82d169f486f32
baguchan <shurf012@gmail.com>
2021-10-23 16:15:40 +0900

    temporary slingshot

--- src/main/java/com/github/teamfusion/greedandbleed/common/registry/ItemRegistry.java
+++ src/main/java/com/github/teamfusion/greedandbleed/common/registry/ItemRegistry.java
@@ -47,23 +47,25 @@ public class ItemRegistry {
                             (new Item.Properties())
                             .stacksTo(1)
                             .tab(TAB_GREED_AND_BLEED))
-    );
+	);
 
-    public static final RegistryObject<Item> HOGLIN_SADDLE = ITEMS.register(
-            "hoglin_saddle", () ->
-                    new HoglinSaddleItem(
-                            (new Item.Properties())
-                                    .stacksTo(1)
-                                    .tab(TAB_GREED_AND_BLEED))
-    );
+	public static final RegistryObject<Item> HOGLIN_SADDLE = ITEMS.register(
+			"hoglin_saddle", () ->
+					new HoglinSaddleItem(
+							(new Item.Properties())
+									.stacksTo(1)
+									.tab(TAB_GREED_AND_BLEED))
+	);
 
-    // COPIED FROM INFERNAL EXPANSION
-    public static final RegistryObject<Item> RAW_HOGCHOP = ITEMS.register("raw_hogchop", () -> new Item(new Item.Properties().tab(TAB_GREED_AND_BLEED)
-            .food(new Food.Builder().nutrition(4).saturationMod(0.3F).meat().build())));
+	public static final RegistryObject<Item> SLINGSHOT = ITEMS.register("slingshot", () -> new Item(new Item.Properties().tab(TAB_GREED_AND_BLEED)));
 
-    public static final RegistryObject<Item> COOKED_HOGCHOP = ITEMS.register("cooked_hogchop", () -> new Item(new Item.Properties().tab(TAB_GREED_AND_BLEED)
-            .food(new Food.Builder().nutrition(10).saturationMod(0.8F).meat().build())));
+	// COPIED FROM INFERNAL EXPANSION
+	public static final RegistryObject<Item> RAW_HOGCHOP = ITEMS.register("raw_hogchop", () -> new Item(new Item.Properties().tab(TAB_GREED_AND_BLEED)
+			.food(new Food.Builder().nutrition(4).saturationMod(0.3F).meat().build())));
 
-    public static final RegistryObject<Item> SKELETAL_PIGLIN_SPAWN_EGG = ITEMS.register("skeletal_piglin_spawn_egg", () -> new SpawnEggItem(EntityTypeRegistry.SKELETAL_PIGLIN_TYPE, 12698049, 4802889, new Item.Properties().tab(TAB_GREED_AND_BLEED)));
-    public static final RegistryObject<Item> PIGMY_SPAWN_EGG = ITEMS.register("pigmy_spawn_egg", () -> new SpawnEggItem(EntityTypeRegistry.PIGMY_TYPE, 10051392, 16380836, new Item.Properties().tab(TAB_GREED_AND_BLEED)));
+	public static final RegistryObject<Item> COOKED_HOGCHOP = ITEMS.register("cooked_hogchop", () -> new Item(new Item.Properties().tab(TAB_GREED_AND_BLEED)
+			.food(new Food.Builder().nutrition(10).saturationMod(0.8F).meat().build())));
+
+	public static final RegistryObject<Item> SKELETAL_PIGLIN_SPAWN_EGG = ITEMS.register("skeletal_piglin_spawn_egg", () -> new SpawnEggItem(EntityTypeRegistry.SKELETAL_PIGLIN_TYPE, 12698049, 4802889, new Item.Properties().tab(TAB_GREED_AND_BLEED)));
+	public static final RegistryObject<Item> PIGMY_SPAWN_EGG = ITEMS.register("pigmy_spawn_egg", () -> new SpawnEggItem(EntityTypeRegistry.PIGMY_TYPE, 10051392, 16380836, new Item.Properties().tab(TAB_GREED_AND_BLEED)));
 }
