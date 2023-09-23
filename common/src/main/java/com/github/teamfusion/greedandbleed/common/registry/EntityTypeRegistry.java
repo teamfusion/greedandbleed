10b1add795557de2cbde080a46d484d21c06f446
baguchan <shutux500@gmail.com>
2023-09-23 11:26:23 +0900

    oops i forgot fire immune

--- common/src/main/java/com/github/teamfusion/greedandbleed/common/registry/EntityTypeRegistry.java
+++ common/src/main/java/com/github/teamfusion/greedandbleed/common/registry/EntityTypeRegistry.java
@@ -18,7 +18,7 @@ public class EntityTypeRegistry {
 
     public static final Supplier<EntityType<SkeletalPiglin>> SKELETAL_PIGLIN = create("skeletal_piglin", EntityType.Builder.of(SkeletalPiglin::new, MobCategory.MONSTER).sized(0.6F, 1.95F));
     public static final Supplier<EntityType<Hoglet>> HOGLET = create("hoglet", EntityType.Builder.of(Hoglet::new, MobCategory.MONSTER).sized(0.6F, 0.55F));
-    public static final Supplier<EntityType<Zoglet>> ZOGLET = create("zoglet", EntityType.Builder.of(Zoglet::new, MobCategory.MONSTER).sized(0.6F, 0.55F));
+    public static final Supplier<EntityType<Zoglet>> ZOGLET = create("zoglet", EntityType.Builder.of(Zoglet::new, MobCategory.MONSTER).fireImmune().sized(0.6F, 0.55F));
     public static final Supplier<EntityType<Skoglet>> SKOGLET = create("skoglet", EntityType.Builder.of(Skoglet::new, MobCategory.MONSTER).sized(0.6F, 0.55F));
 
     private static <T extends Entity> Supplier<EntityType<T>> create(String key, EntityType.Builder<T> builder) {
