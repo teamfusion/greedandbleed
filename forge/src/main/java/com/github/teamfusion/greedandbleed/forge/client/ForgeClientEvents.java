858f9bb7bf1f787b7d4b5f01b922ea75ad40410e
baguchan <shutux500@gmail.com>
2023-11-05 15:44:10 +0900

    fix ToleratingOverlay

--- forge/src/main/java/com/github/teamfusion/greedandbleed/forge/client/ForgeClientEvents.java
+++ forge/src/main/java/com/github/teamfusion/greedandbleed/forge/client/ForgeClientEvents.java
@@ -1,14 +1,7 @@
 package com.github.teamfusion.greedandbleed.forge.client;
 
-import com.github.teamfusion.greedandbleed.client.RenderHelper;
-import com.github.teamfusion.greedandbleed.common.entity.ToleratingMount;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
-import net.minecraftforge.client.gui.overlay.NamedGuiOverlay;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -20,19 +13,5 @@ public class ForgeClientEvents {
     @SubscribeEvent
     public static void onClientInput(InputEvent event) {
         //TODO: Make mounted hoglins attack on input
-    }
-
-    @SubscribeEvent
-    public static void onRenderingXPBar(RenderGuiOverlayEvent.Pre event) {
-        NamedGuiOverlay type = event.getOverlay();
-        if (type == VanillaGuiOverlay.EXPERIENCE_BAR.type()) {
-            Minecraft minecraft = Minecraft.getInstance();
-            LocalPlayer player = minecraft.player;
-            if (player != null && player.getVehicle() instanceof ToleratingMount) {
-                event.setCanceled(true);
-                ToleratingMount toleratingMount = (ToleratingMount) player.getVehicle();
-                RenderHelper.renderToleranceMeter(toleratingMount, event.getGuiGraphics());
-            }
-        }
     }
 }
