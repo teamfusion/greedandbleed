7e2f4b90569aa23fa221d62a6936b6c7ffa0dc7b
baguchan <shutux500@gmail.com>
2024-10-16 08:46:40 +0900

    now with gui

--- common/src/main/java/com/github/teamfusion/greedandbleed/common/block/PygmyStationBlock.java
+++ common/src/main/java/com/github/teamfusion/greedandbleed/common/block/PygmyStationBlock.java
@@ -1,15 +1,39 @@
 package com.github.teamfusion.greedandbleed.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 public class PygmyStationBlock extends BaseEntityBlock {
     public PygmyStationBlock(Properties properties) {
         super(properties);
+    }
+
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        if (level.isClientSide) {
+            return InteractionResult.SUCCESS;
+        } else {
+            this.openContainer(level, blockPos, player);
+            return InteractionResult.CONSUME;
+        }
+    }
+
+
+    protected void openContainer(Level level, BlockPos blockPos, Player player) {
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (blockEntity instanceof PygmyStationBlockEntity) {
+            player.openMenu((MenuProvider) blockEntity);
+        }
+
     }
 
     public RenderShape getRenderShape(BlockState blockState) {
